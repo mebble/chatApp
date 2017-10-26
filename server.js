ce1e1d0dbb2125895a92d19e3c786ad37c71fb49
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-10-26 13:56:42 +0530

    update users list in server and client

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ io.sockets.on('connection', (socket) => {
   socket.on('disconnect', (data) => {
     connections.splice(connections.indexOf(socket), 1);
     console.log(`Disconnected: ${connections.length} sockets connected`);
+
+    if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);  // users.indexOf(undefined) == -1
+    updateUsernames();
   });
 
   // message from client
@@ -33,6 +37,11 @@ io.sockets.on('connection', (socket) => {
     callback(true);
     socket.username = data;  // this socket connection gets the socket user's username
     users.push(socket.username);
-    io.sockets.emit('get users', users);
+    updateUsernames();
   });
+
+  // let the clients know the updated users list
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
 });
